e9fdbc86fc89153668801977e4ba907722d90603
ntduonglib <nguyentungduong.9x1@gmail.com>
2013-11-24 19:42:39 +0700

    Created
    Created

---
+++ com/koushikdutta/urlimageviewhelper/FileUrlDownloader.java
@@ -0,0 +1,44 @@
+package com.koushikdutta.urlimageviewhelper;
+
+import java.io.File;
+import java.net.URI;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+public class FileUrlDownloader implements UrlDownloader {
+	@Override
+	public void download(final Context context, final String url, final String filename, final UrlDownloaderCallback callback,
+			final Runnable completion) {
+		final AsyncTask<Void, Void, Void> downloader = new AsyncTask<Void, Void, Void>() {
+			@Override
+			protected Void doInBackground(final Void... params) {
+				try {
+					File f = new File(new URI(url));
+					callback.onDownloadComplete(FileUrlDownloader.this, null, f.getAbsolutePath(), f.length());
+					return null;
+				} catch (final Throwable e) {
+					e.printStackTrace();
+					return null;
+				}
+			}
+
+			@Override
+			protected void onPostExecute(final Void result) {
+				completion.run();
+			}
+		};
+
+		UrlImageViewHelper.executeTask(downloader);
+	}
+
+	@Override
+	public boolean allowCache() {
+		return false;
+	}
+
+	@Override
+	public boolean canDownloadUrl(String url) {
+		return url.startsWith("file:/");
+	}
+}
---
+++ com/koushikdutta/urlimageviewhelper/UrlDownloader.java
@@ -0,0 +1,18 @@
+package com.koushikdutta.urlimageviewhelper;
+
+import java.io.InputStream;
+
+import android.content.Context;
+
+public interface UrlDownloader {
+	public static interface UrlDownloaderCallback {
+		public void onDownloadComplete(UrlDownloader downloader, InputStream in, String filename, long fileLength);
+
+	}
+
+	public void download(Context context, String url, String filename, UrlDownloaderCallback callback, Runnable completion);
+
+	public boolean allowCache();
+
+	public boolean canDownloadUrl(String url);
+}
